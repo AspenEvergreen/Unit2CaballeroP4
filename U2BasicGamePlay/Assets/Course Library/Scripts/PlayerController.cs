457017c4fafd902aeca91c791d52d58ac1e2e559
AspenTree <719391@student.dusd.net>
2025-03-26 11:41:17 -0700

    do bonuses

--- U2BasicGamePlay/Assets/Course Library/Scripts/PlayerController.cs
+++ U2BasicGamePlay/Assets/Course Library/Scripts/PlayerController.cs
@@ -6,10 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     // for movement
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
 
     // for boundaries
     public float xRange = 20;
+    public float zRangeTop = 15;
+    public float zRangeBottom = 0;
 
     //projectile
     public GameObject projectilePrefab;
@@ -29,8 +32,13 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
         // keep the player in
 
+        // x axis
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -41,10 +49,26 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        // y axis
+        if(transform.position.z > zRangeTop)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeTop);
+        }
+
+        if (transform.position.z < zRangeBottom)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRangeBottom);
+        }
+
         // launch projectiles from player
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
+
+        //if (gameObject.name == ("Player" is Destroy))
+        //{
+        //    Debug.Log("Game Over");
+        //}
     }
 }
--- U2BasicGamePlay/Assets/Course Library/Scripts/SpawnManager.cs
+++ U2BasicGamePlay/Assets/Course Library/Scripts/SpawnManager.cs
@@ -8,15 +8,20 @@ public class SpawnManager : MonoBehaviour
     public int animalIndex;
 
     private float spawnRangeX = 20;
-    private float spawnPosZ = 20;
+    private float spawnRangeZ = 20;
+    
+    private float SPZ = 20;
+    private float SPX = 20;
 
-    private float startDelay = 2;
-    private float spawnInterval = 1.5f;
+    private float startDelay = 1;
+    private float spawnInterval = 0.66f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomAnimalTop", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomAnimalLeft", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomAnimalRight", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -25,14 +30,37 @@ public class SpawnManager : MonoBehaviour
 
     }
 
-    void SpawnRandomAnimal()
+    void SpawnRandomAnimalTop()
     {
         // randomly spawn animals
 
         int animalIndex = Random.Range(0, animalPrefabs.Length);
 
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, SPZ);
 
         Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
+
+    void SpawnRandomAnimalLeft()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Vector3 spawnPos = new Vector3(-SPX, 0, Random.Range(0, spawnRangeZ));
+
+        Quaternion rotationLeft = Quaternion.Euler(0, 90, 0);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, rotationLeft);
+    }
+
+    void SpawnRandpmAnimalRight()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Vector3 spawnPos = new Vector3(-SPX, 0, Random.Range(0, spawnRangeZ));
+
+        Quaternion rotationRight = Quaternion.Euler(0, -90, 0);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, rotationRight);
+    }
 }
+    
